2941c7834fb203325c4db007b159ea6b430b727a
Kaleb Molewe <88307946+KalebMolewe23@users.noreply.github.com>
2025-10-11 19:14:47 +0700

    update logout button

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -16,7 +16,7 @@
         <ul class="profile-link">
             <li><a href="#"><i class='bx bxs-user-circle icon' ></i> Profile</a></li>
             <li><a href="#"><i class='bx bxs-cog' ></i> Settings</a></li>
-            <li><a href="#"><i class='bx bxs-log-out-circle' ></i> Logout</a></li>
+            <li><a href="{{ route('logout') }}"><i class='bx bxs-log-out-circle' ></i> Logout</a></li>
         </ul>
     </div>
 </nav>

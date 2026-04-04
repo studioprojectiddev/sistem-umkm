e2857ecf2e944b67c4f0ba7844450cba9ab6042a
faisal-oktavian <oktavianfaisal97@gmail.com>
2026-04-04 08:32:21 +0700

    update stock report

--- resources/views/exports/stok_report.blade.php
+++ resources/views/exports/stok_report.blade.php
@@ -3,11 +3,13 @@
         <tr>
             <th>Tanggal</th>
             <th>Produk</th>
+            <th>Gudang</th>
             <th>Variasi</th>
             <th>Stok Awal</th>
             <th>Stok Masuk</th>
             <th>Stok Keluar</th>
             <th>Saldo Akhir</th>
+            <th>Min Stock</th>
             <th>HPP</th>
             <th>Nilai Stok</th>
             <th>Potensi Laba</th>
@@ -18,14 +20,16 @@
             <tr>
                 <td>{{ optional($item->created_at) ? \Carbon\Carbon::parse($item->created_at)->format('d/m/Y') : '-' }}</td>
                 <td>{{ $item->product?->name ?? '-' }}</td>
+                <td>{{ $item->warehouse_name ?? '-' }}</td>
                 <td>{{ $item->variation?->name ?? '-' }}</td>
                 <td>{{ $item->stok_awal ?? 0 }}</td>
                 <td>{{ $item->stok_masuk ?? 0 }}</td>
                 <td>{{ $item->stok_keluar ?? 0 }}</td>
-                <td>{{ $item->saldo ?? 0 }}</td>
-                <td>{{ number_format($item->hpp ?? 0, 0, ',', '.') }}</td>
-                <td>{{ number_format($item->nilai_stok ?? 0, 0, ',', '.') }}</td>
-                <td>{{ number_format($item->potensi_laba ?? 0, 0, ',', '.') }}</td>
+                <td>{{ $item->stok_akhir ?? 0 }}</td>
+                <td>{{ $item->min_stock ?? 0 }}</td>
+                <td>{{ $item->hpp ?? 0 }}</td>
+                <td>{{ $item->nilai_stok ?? 0 }}</td>
+                <td>{{ $item->potensi_laba ?? 0 }}</td>
             </tr>
         @endforeach
     </tbody>

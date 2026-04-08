5c20f7ede3922f586e8fbc2b6d79ac9b89178530
faisal-oktavian <74882640+faisal-oktavian@users.noreply.github.com>
2026-04-08 07:52:13 +0700

    update menu cashflow report

--- resources/views/exports/arus_kas.blade.php
+++ resources/views/exports/arus_kas.blade.php
@@ -24,15 +24,15 @@
                 <tr>
                     <td></td>
                     <td>{{ $item->description }}</td>
-                    <td>{{ $item->income ? $item->income, 0, ',', '.' : 0 }}</td>
-                    <td>{{ $item->expense ? $item->expense, 0, ',', '.' : 0 }}</td>
+                    <td>{{ $item->income ? $item->income : 0 }}</td>
+                    <td>{{ $item->expense ? $item->expense : 0 }}</td>
                 </tr>
             @endforeach
             <tr>
                 <td></td>
                 <td><strong>Total {{ $category['label'] }}</strong></td>
-                <td><strong>{{ $category['income'], 0, ',', '.' }}</strong></td>
-                <td><strong>{{ $category['expense'], 0, ',', '.' }}</strong></td>
+                <td><strong>{{ $category['income'] }}</strong></td>
+                <td><strong>{{ $category['expense'] }}</strong></td>
             </tr>
         @endforeach
         <tr>
@@ -40,23 +40,23 @@
         </tr>
         <tr>
             <td colspan="2"><strong>Total Kas Masuk</strong></td>
-            <td colspan="2"><strong>{{ $report['total_income'], 0, ',', '.' }}</strong></td>
+            <td colspan="2"><strong>{{ $report['total_income'] }}</strong></td>
         </tr>
         <tr>
             <td colspan="2"><strong>Total Kas Keluar</strong></td>
-            <td colspan="2"><strong>{{ $report['total_expense'], 0, ',', '.' }}</strong></td>
+            <td colspan="2"><strong>{{ $report['total_expense'] }}</strong></td>
         </tr>
         <tr>
             <td colspan="2"><strong>Kenaikan / Penurunan Kas</strong></td>
-            <td colspan="2"><strong>{{ $report['net_change'], 0, ',', '.' }}</strong></td>
+            <td colspan="2"><strong>{{ $report['net_change'] }}</strong></td>
         </tr>
         <tr>
             <td colspan="2"><strong>Saldo Kas Awal</strong></td>
-            <td colspan="2"><strong>{{ $report['opening_balance'], 0, ',', '.' }}</strong></td>
+            <td colspan="2"><strong>{{ $report['opening_balance'] }}</strong></td>
         </tr>
         <tr>
             <td colspan="2"><strong>Saldo Kas Akhir</strong></td>
-            <td colspan="2"><strong>{{ $report['ending_balance'], 0, ',', '.' }}</strong></td>
+            <td colspan="2"><strong>{{ $report['ending_balance'] }}</strong></td>
         </tr>
     </tbody>
 </table>
